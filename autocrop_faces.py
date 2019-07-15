9b9d0ce88ba73fc26126d3246fd53e8e75b427b6
Cyril Tom Mathew <52824333+ctoms920@users.noreply.github.com>
2019-07-15 07:00:49 +0100

    Updated

--- autocrop_faces.py
+++ autocrop_faces.py
@@ -2,16 +2,14 @@ import cv2
 from random import*
 import os
 
-
 directory = "C:\\Users\Cyril Tom Mathew\Desktop\images_test"
-for filename in os.listdir(directory):
-    if filename.endswith(".jpg") or filename.endswith(".png"): 
-        image = cv2.imread(os.path.join(directory, filename))
-        face_cascade = cv2.CascadeClassifier("C:\\Users\Cyril Tom Mathew\Desktop\haarcascade_frontalface_default.xml")
+face_cascade = cv2.CascadeClassifier("C:\\Users\Cyril Tom Mathew\Desktop\haarcascade_frontalface_default.xml")
 
+for filename in os.listdir(directory):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        image = cv2.imread(os.path.join(directory, filename))
         gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray_img, 1.38, 5)
-
 
         for (x, y, w, h) in faces:
             img_face = image[y:y+h, x:x+w]
